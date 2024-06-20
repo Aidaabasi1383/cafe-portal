223a066dcec5134f174d538d13fe34922a9f1a62
cloner174 <hamedhajipour2012@hotmail.com>
2024-06-21 01:36:10 +0330

    adding nessesery models and serilize

---
+++ cafe_portal_backend/api/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    full_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    sugar = models.IntegerField()
+    coffee = models.IntegerField()
+    flour = models.IntegerField()
+    chocolate = models.IntegerField()
+    vertical = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    purchase_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.BooleanField()
+
+class Storage(models.Model):
+    name = models.CharField(max_length=255)
+    amount = models.IntegerField()
