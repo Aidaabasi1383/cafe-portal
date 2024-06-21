47deb6bf041ab40cef05f30fa932fd367b0e24cf
cloner174 <hamedhajipour2012@hotmail.com>
2024-06-21 03:47:54 +0330

    create and modify models and views.py

--- cafe_portal_backend/api/views.py
+++ cafe_portal_backend/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Product, Order, Storage
+from .serializers import UserSerializer, ProductSerializer, OrderSerializer, StorageSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class StorageViewSet(viewsets.ModelViewSet):
+    queryset = Storage.objects.all()
+    serializer_class = StorageSerializer
